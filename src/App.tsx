4162f16c8f72ac8bd80a072cfef2898557648c27
Sean Newell <sean@thenewells.us>
2022-11-29 23:24:26 +0000

    Setup themes and a theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+
+// Theme provider is the HTML DOM Node's data-theme attribute
+const $htmlElement = document.documentElement;
+
+function setTheme(themeName: string) {
+  $htmlElement.setAttribute('data-theme', themeName);
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [darkThemeOn, setDarkThemeOn] = useState(true);
 
   function increment() { setCount(count + 1); };
+  function toggleDarkTheme() {
+    setDarkThemeOn(!darkThemeOn);
+  };
+
+  useEffect(
+    () => setTheme(darkThemeOn ? "dark": "light"),
+    [darkThemeOn]
+  );
 
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-lg">
-          <h1 className="text-5xl font-bold w-full">Vite + React + Typescript + TailwindCSS + DaisyUI</h1>
-          <button className="mt-8 btn btn-primary" onClick={increment}>Count is {count}</button>
-          <p className="mt-4 text-teal-300">Rocket to the Moon!</p>
+    <div className="relative">
+      <div className="absolute top-4 right-4 w-fit">
+        <input title="Toggle Dark/Light Themes" type="checkbox" className="toggle" checked={darkThemeOn} onChange={toggleDarkTheme} />
+      </div>
+      <div className="hero min-h-screen bg-neutral">
+        <div className="hero-content text-center">
+          <div className="max-w-lg">
+            <h1 className="text-5xl tracking-tighter font-bold w-full text-primary font-interBold">Vite + React + Typescript + TailwindCSS + DaisyUI</h1>
+            <button className="mt-8 btn btn-primary font-inter tracking-tightest" onClick={increment}>Count is {count}</button>
+            <p className="mt-4 text-secondary tracking-tightest">Rocket to the Moon!</p>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          "primary": "#F2F2F2",
+          "secondary": "#B0B0B0",
+          "accent": "#383838",
+          "neutral": "#151515",
+          "base-100": "#222222",
+          "info": "#0072F5",
+          "success": "#21CA51",
+          "warning": "#FF6052",
+          "error": "#E75B4F",
+        },
+        light: {
+          "primary": "#151515",
+          "secondary": "#515151",
+          "accent": "#D1D1D1",
+          "neutral": "#FFFFFF",
+          "base-100": "#EEEEEE",
+          "info": "#0072F5",
+          "success": "#21CA51",
+          "warning": "#FF6052",
+          "error": "#D62617",
+        }
+      }
+    ]
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['InterRegular'],
+        interBold: ['InterSemiBold'],
+      },
+      letterSpacing: {
+        tightest: '-0.0125em'
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
